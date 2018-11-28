b1a25ed5a8106770c94dc0abcf36d0b6f16d3c50
Srkline3 <klinesr@rose-hulman.edu>
2018-11-28 16:53:29 -0500

    ATTENTION ALL CSSE 120 L33T C0D3RZ
    Trey Kline needs your help to debug his code. All you have to do is hit that commit button. But act fast so he can secure the bag and get that A.

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -4,11 +4,11 @@ Demonstrates using (calling) FUNCTIONS and using (calling) METHODS:
   -- how they differ.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Trey Kline.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   With your instructor, READ the file   methods_vs_functions.txt
 #   in this project, ASKING QUESTIONS as needed to understand its contents.
 #   After you have done so, mark this _TODO_ as DONE
@@ -98,7 +98,7 @@ def draw_many_squares(my_turtle, number_of_squares, size, twist):
         my_turtle.left(twist)
 
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   There are four FUNCTIONS defined ABOVE this:
 #     main
 #     run_example
@@ -136,10 +136,15 @@ def try_methods():
       -- backward  100 units
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function, per its doc-string above.
+    # DONE: 4. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     ###########################################################################
-
+    myMom = rg.SimpleTurtle()
+    myMom.pen = rg.Pen('brown', 5)
+    myMom.forward(150)
+    myMom.left(90)
+    myMom.forward(50)
+    myMom.backward(100)
 
 ###############################################################################
 # IMPORTANT: Read the NOTE below before you try to implement the next function!
@@ -152,7 +157,7 @@ def try_functions():
      -- One jumps to (-50, 50), then moves (while drawing) to (100, 100)
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function, per its doc-string above.
+    # DONE: 5. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #    NOTE: This function requires
@@ -160,7 +165,9 @@ def try_functions():
     #    If you think it needs more, ** ASK FOR HELP. **
     #    HINT: see   jump_and_move_turtle   above.
     ###########################################################################
-
+    jump_and_move_turtle(200, 100, 300, 30)
+    jump_and_move_turtle(100, 200, 0, 0)
+    jump_and_move_turtle(-50, 50, 100, 100)
 
 ###############################################################################
 # IMPORTANT: Read the NOTE below before you try to implement the next function!
@@ -198,13 +205,28 @@ def try_methods_and_functions():
       8. Draw a SQUARE whose sides are each of length 50.
     """
     ###########################################################################
-    # TODO: 6. Implement and test this function, per its doc-string above.
+    # DONE: 6. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #   NOTE: This function should ** CALL ** the
     #     draw_many_squares
     #   function defined above.  If you don't see why, ** ASK FOR HELP. **
     ###########################################################################
+    myOtherMom = rg.SimpleTurtle()
+    myOtherMom.pen = rg.Pen('blue', 5)
+    myOtherMom.backward(150)
+    myOtherMom.speed = 1
+    draw_many_squares(myOtherMom, 2, 100, 30)
+    myOtherMom.speed = 5
+    myOtherMom.pen = rg.Pen('red',5)
+    draw_many_squares(myOtherMom, 10, 50, 15)
+    myOtherMom.speed = 100
+    myOtherMom.pen = rg.Pen('red', 35)
+    draw_many_squares(myOtherMom, 8, 300, 60)
+    myOtherMom.pen = rg.Pen('black', 3)
+    myOtherMom.backward(200)
+    myOtherMom.draw_circle(30)
+    myOtherMom.draw_square(50)
 
 
 # -----------------------------------------------------------------------------
